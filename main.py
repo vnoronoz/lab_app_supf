27c18b111da8fd11c88af8850d7123c81e6bee99
vnoronoz <66873581+vnoronoz@users.noreply.github.com>
2024-01-19 11:59:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -41,12 +41,12 @@ if LAB_FILE is not None:
 
         QUERY_EST = """SELECT est, nombre FROM red_calidad.estaciones_rios"""
         QUERY_HISTORIC ="""SELECT est, ph_lab, cond_lab, mat_org, cl, so4, no3, no2, nh4, ptot, po4, solidos_susp, tic, toc, dbo5, e_coli, coliformes_totales, dureza, ca, mg, co3, co3h, na, k, as_, cd, cr, cu, fe, hg, mn, ni, pb, se, zn FROM red_calidad.historic_rios"""
-        data_est = pd.read_csv('estaciones_rios.csv', engine='openpyxl'))
+        data_est = pd.read_csv('estaciones_rios.csv')
         data_est = data_est[['est','nombre']]
-        data_hist = pd.read_csv('historic_rios.csv', engine='openpyxl'))
+        data_hist = pd.read_csv('historic_rios.csv')
         data_hist = data_hist[['est','ph_lab','cond_lab','mat_org','cl','so4','no3','no2','nh4','ptot','po4','solidos_susp','tic','toc','dbo5','e_coli','coliformes_totales','dureza','ca','mg','co3','co3h','na','k','as_','cd','cr','cu','fe','hg','mn','ni','pb','se','zn']]
 
-        data = pd.read_excel(LAB_FILE, converters={'Código':str}, engine='openpyxl'))
+        data = pd.read_excel(LAB_FILE, converters={'Código':str}, engine='openpyxl')
         data_r = f.rename_cols_original_file(data)
         df_analysis = f.file_processing(data_r)
         f.replace_comma(df_analysis, PROCESSING_COLS)
